943733f43509901bdf45e73a8259ac1f2dd7fda5
Michael Bernd Beier <code@michaelberndbeier.com>
2021-09-25 20:12:27 +0200

    Simple Swift implementation finished

--- SecretSantaSwiftCli/main.swift
+++ SecretSantaSwiftCli/main.swift
@@ -7,5 +7,29 @@
 
 import Foundation
 
-print("Hello, World!")
+var participants = ["Michael", "Alice", "Bob", "Charlie", "Diane"];
 
+func getOffset(senders: Array<String>)  -> Int
+{
+	var numParticipants = senders.capacity
+	return Int.random(in: 0..<numParticipants)
+}
+
+func getReceiverIndex(senderIndex: Int, numParticipants: Int, offset: Int) -> Int{
+	var relativeIndex = senderIndex + offset;
+	var adjustedIndex = relativeIndex % numParticipants;
+	
+	return adjustedIndex
+}
+func getPairings(senders: Array<String>) -> [(sender: String, receiver: String)]
+{
+	var pairings: [(sender: String, receiver: String)] = []
+	var offset = getOffset(senders: participants)
+	for (index, element) in participants.enumerated() {
+		pairings.append((sender:element, receiver:participants[getReceiverIndex(senderIndex: index, numParticipants: participants.capacity, offset: offset)]))
+	}
+	
+	return pairings;
+}
+
+print(getPairings(senders: participants))
